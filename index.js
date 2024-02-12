7c4a90ebac61f4a65914df86de4cbbcce1d27354
Calvin Ellis <cellis9421@gmail.com>
2024-02-12 15:20:27 -0500

    Add test logging

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var domains = ["stoveandgrillparts.com", "pellet-stove-parts-4less.com"];
+
+    // Check if we have the "test" parameter in the URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var isTest = urlParams.has('test');
 if (!domains.includes(window.location.hostname)) {
-  var meta = document.createElement('meta');
-  meta.name = "robots";
-  meta.content = "noindex";
-  document.getElementsByTagName('head')[0].appendChild(meta);
+
+    // If the "test" parameter is present, add a console log of the result
+    if (isTest) {
+        console.log("*** THIS IN AN UNAUTHORIZED DOMAIN ***");
+    }
+    var meta = document.createElement('meta');
+    meta.name = "robots";
+    meta.content = "noindex";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+}
+
+if (!isTest) {
+    console.log("This domain is OK.");
 }
